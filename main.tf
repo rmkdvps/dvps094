5999362a2054a5428ad6d0cc83cf64e8d5db068f
Roman Moisa <71345414+rmkdvps@users.noreply.github.com>
2021-09-13 20:29:17 +0300

    test terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,130 @@
+# =============== networks and firewall =================
+
 resource "google_compute_network" "vpc_network" {
-  name = "test2-vpc"
+  name = "${var.project}-vpc"
   auto_create_subnetworks = false
+  project = var.project
+}
+
+resource "google_compute_subnetwork" "vpc_subnetwork" {
+  name          = "${var.project}-subnet"
+  ip_cidr_range = "10.10.0.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.name
+  project = var.project
+}
+
+resource "google_compute_firewall" "firewall_rules" {
+  project = var.project
+  name    = "firewall-rules"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports = ["80", "444", "55000", "5000", "5001", "5671", "5672", "5673", "15672", "8080"]
+  }
+
+  target_tags = ["app"]
+}
+
+# =============== db =================
+resource "random_id" "db_name" {
+  byte_length = 8
+}
+
+resource "google_sql_database" "post_db" {
+  name     = random_id.db_name.hex
+  instance = google_sql_database_instance.machine.name
+}
+
+resource "random_id" "db_instance_name" {
+  byte_length = 3
+}
+
+resource "google_sql_database_instance" "machine" {
+  # name             = "db_machine-${random_id.db_instance_name.hex}"
+  database_version = "POSTGRES_13"
+  region           = var.region
+  project          = var.project
+
+  settings {
+    tier = "db-f1-micro"
+    disk_size = var.disk_size
+  }
+
+  deletion_protection  = "false"
+
+}
+
+resource "google_sql_user" "postgresql_user" {
+  name = var.db_user
+  project  = var.project
+  instance = google_sql_database_instance.machine.name
+  password = var.db_password
+}
+
+# =============== GKE =================
+
+resource "google_container_cluster" "primary" {
+  name     = "${var.project}-gke-cluster"
+  location = var.location
+  
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc_network.name
+  subnetwork = google_compute_subnetwork.vpc_subnetwork.name
+}
+
+data "google_client_config" "default" {}
+
+# provider "kubernetes" {
+  
+#   host     = "https://${data.google_container_cluster.primary.endpoint}"
+#   token                  = data.google_client_config.default.access_token
+#   cluster_ca_certificate = data.google_container_cluster.primary.cluster_ca_certificate
+# }
+
+# provider "kubernetes" {
+#   load_config_file = false
+
+#   host  = "https://${data.google_container_cluster.primary.endpoint}"
+#   token = data.google_client_config.default.access_token
+#   cluster_ca_certificate = base64decode(
+#     data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
+#   )
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     host = "https://${data.google_container_cluster.primary.endpoint}"
+
+#   }
+# }
+
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${google_container_cluster.primary.name}-node-pool"
+  location   = var.location
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project
+    }
+
+    # preemptible  = true
+    machine_type = "e2-medium"
+    tags         = ["gke-node", "${var.project}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,59 @@
+# =============== provider =================
 variable "region" {
-    default = "europe-central2"
+  description = "gcp region"
+  default     = "europe-central2"
 }
 
+variable "project" {
+  description = "name of the project"
+  default     = "softserve-project"
+}
+
+variable "project_id" {
+  description = "name of the project"
+  default     = "softserve-project"
+}
+
+variable "zone" {
+  description = "gcp zone"
+  default     = "europe-central2-a"
+}
+
+variable "location" {
+  description = "gcp location"
+  default     = "europe-central2-a"
+}
+
+# =============== db =================
+
+variable "machine_type" {
+  description = "instance macine type"
+  default     = "e2-micro"
+}
+
+variable "disk_size" {
+  description = "db disk_size"
+  default     = "10"
+}
+
+variable "db_user" {
+  description = "db user"
+  default     = "postgresql"
+}
+
+variable "db_password" {
+  description = "db pass"
+  default     = "postgresql"
+}
+
+# =============== GKE =================
+
+variable "cluster_name" {
+  description = "k8s cluster_name"
+  default     = "dev094"
+}
+
+# =============== GCB =================
 variable "github_repository" {
     default = "dvps094"
 }
@@ -12,12 +64,4 @@ variable "github_owner" {
 
 variable "github_branch" {
     default = "^master$"
-}
-variable "project" {
-  description = "name of the project"
-  default     = "softserve-project"
-}
-variable "project_id" {
-  description = "project id"
-  default     = "softserve-project"
 }
